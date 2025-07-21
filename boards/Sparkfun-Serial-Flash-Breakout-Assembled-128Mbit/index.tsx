f9f6cd598f65bdff9eade629a4b742dc05900c91
Abse2001 <abdaslam13.2.2001@gmail.com>
2025-07-21 20:05:03 +0200

    fix

--- boards/Sparkfun-Serial-Flash-Breakout-Assembled-128Mbit/index.tsx
+++ boards/Sparkfun-Serial-Flash-Breakout-Assembled-128Mbit/index.tsx
@@ -1,3 +1,3 @@
-import W25Q128JVPIQ from "./Sparkfun-Serial-Flash-Breakout-Assembled-128Mbit"
+import W25Q128JVPIQ from "./Sparkfun-Serial-Flash-Breakout-Assembled-128Mbit.circuit.tsx"
 
 export default W25Q128JVPIQ
