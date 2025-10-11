5799a9c3ba33046c476aa41584819b38043c8280
YadavAkhileshh <yadavakhil2501@gmail.com>
2025-10-12 02:57:58 +0530

    Fix syntax and structure

--- boards/SparkFun-MicroMod-STM32-Processor/SparkFun-MicroMod-STM32-Processor.circuit.tsx
+++ boards/SparkFun-MicroMod-STM32-Processor/SparkFun-MicroMod-STM32-Processor.circuit.tsx
@@ -1,25 +1,16 @@
+import { sel } from "tscircuit"
+
 export default () => (
   <board width="22mm" height="16mm">
     <chip
       name="U1"
+      cadModel={null}
       footprint="lqfp64"
       pcbX={0}
       pcbY={0}
+      schX={0}
+      schY={0}
     />
-    <resistor
-      name="R1"
-      resistance="10k"
-      footprint="0603"
-      pcbX={5}
-      pcbY={5}
-    />
-    <capacitor
-      name="C1"
-      capacitance="0.1uf"
-      footprint="0603"
-      pcbX={-5}
-      pcbY={5}
-    />
-    <silkscreentext text="STM32" fontSize={1.5} pcbX={-8} pcbY={-6} />
+    <silkscreentext text="STM32" pcbX={-8} pcbY={-6} />
   </board>
 )
