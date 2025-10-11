d91c7bb920ad911c78204f183f5168b5c707b68f
YadavAkhileshh <yadavakhil2501@gmail.com>
2025-10-12 03:05:44 +0530

    format fix

--- boards/SparkFun-MicroMod-STM32-Processor/SparkFun-MicroMod-STM32-Processor.circuit.tsx
+++ boards/SparkFun-MicroMod-STM32-Processor/SparkFun-MicroMod-STM32-Processor.circuit.tsx
@@ -2,13 +2,7 @@ import { sel } from "@tscircuit/core"
 
 export default () => (
   <board width="22mm" height="16mm">
-    <chip
-      name="U1"
-      cadModel={null}
-      footprint="lqfp64"
-      pcbX={0}
-      pcbY={0}
-    />
+    <chip name="U1" cadModel={null} footprint="lqfp64" pcbX={0} pcbY={0} />
     <silkscreentext text="STM32" pcbY={-6} />
   </board>
 )
