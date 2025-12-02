b6c8c7403f8ac62b6753eddff87383773180a38d
rushabhcodes <mail@rushabh.dev>
2025-12-03 01:26:21 +0530

    Revert "Fix SparkFun Air Velocity Sensor Breakout Board"
    This reverts commit 5a1e4ea77728f4385b5a2b25f02e753ece91a6b6.

--- boards/SparkFun-Air-Velocity-Sensor-Breakout-FS3000-1015/SparkFun-Air-Velocity-Sensor-Breakout-FS3000-1015.circuit.tsx
+++ boards/SparkFun-Air-Velocity-Sensor-Breakout-FS3000-1015/SparkFun-Air-Velocity-Sensor-Breakout-FS3000-1015.circuit.tsx
@@ -1,7 +1,6 @@
 import { FS3000_1015 } from "./ FS3000_1015"
 import { SM04B_SRSS_TB_LF__SN } from "./SM04B_SRSS_TB_LF__SN"
 import { sel } from "tscircuit"
-import { SM04B_SRSS_TB_LF__SN2 } from "./SM04B_SRSS_TB_LF__SN2"
 
 export default () => {
   return (
@@ -456,7 +455,7 @@ export default () => {
         connectsTo={sel.JP6.pin1}
         anchorSide="top"
       />
-      <SM04B_SRSS_TB_LF__SN2
+      <SM04B_SRSS_TB_LF__SN
         name="JP7"
         schX={-4}
         schY={-8}
