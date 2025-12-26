37532c45183cbe65b080b074a5b98880dd643845
SBALAVIGNESH123 <balavignesh449@gmail.com>
2025-12-26 15:24:33 +0530

    style: format files with biome

--- boards/SparkFun-MicroMod-STM32-Processor/STM32F405.tsx
+++ boards/SparkFun-MicroMod-STM32-Processor/STM32F405.tsx
@@ -3,46 +3,46 @@ import type { ChipProps } from "@tscircuit/props"
 // stm32f405rgt6 simplified for micromod
 
 const pinLabels = {
-    pin1: ["VCAP1"],
-    pin2: ["VCAP2"],
-    pin3: ["VDD3"],
-    pin4: ["VDD4"],
-    pin5: ["VDDA"],
-    pin6: ["OSC_IN"],
-    pin7: ["OSC_OUT"],
-    pin8: ["OSC32_IN"],
-    pin9: ["OSC32_OUT"],
-    pin10: ["NRST"],
-    pin11: ["BOOT0"],
-    pin12: ["TX1"],
-    pin13: ["RX1"],
-    pin14: ["SWDIO"],
-    pin15: ["SWCLK"],
-    pin16: ["SCL1"],
-    pin17: ["SDA1"],
-    pin18: ["OTG_FS_DM"],
-    pin19: ["OTG_FS_DP"],
-    pin20: ["SDIO_CK"],
-    pin21: ["SDIO_D0"],
-    pin22: ["SDIO_D1"],
-    pin23: ["SDIO_D2"],
-    pin24: ["SDIO_D3"],
-    pin25: ["NSCS"],
-    pin26: ["MISO1"],
-    pin27: ["MOSI1"],
-    pin28: ["SCK1"],
+  pin1: ["VCAP1"],
+  pin2: ["VCAP2"],
+  pin3: ["VDD3"],
+  pin4: ["VDD4"],
+  pin5: ["VDDA"],
+  pin6: ["OSC_IN"],
+  pin7: ["OSC_OUT"],
+  pin8: ["OSC32_IN"],
+  pin9: ["OSC32_OUT"],
+  pin10: ["NRST"],
+  pin11: ["BOOT0"],
+  pin12: ["TX1"],
+  pin13: ["RX1"],
+  pin14: ["SWDIO"],
+  pin15: ["SWCLK"],
+  pin16: ["SCL1"],
+  pin17: ["SDA1"],
+  pin18: ["OTG_FS_DM"],
+  pin19: ["OTG_FS_DP"],
+  pin20: ["SDIO_CK"],
+  pin21: ["SDIO_D0"],
+  pin22: ["SDIO_D1"],
+  pin23: ["SDIO_D2"],
+  pin24: ["SDIO_D3"],
+  pin25: ["NSCS"],
+  pin26: ["MISO1"],
+  pin27: ["MOSI1"],
+  pin28: ["SCK1"],
 } as const
 
 export const STM32F405 = (props: ChipProps<typeof pinLabels>) => {
-    return (
-        <chip
-            pinLabels={pinLabels}
-            supplierPartNumbers={{
-                jlcpcb: ["C15742"],
-            }}
-            manufacturerPartNumber="STM32F405RGT6"
-            footprint="lqfp64_w10_h10_pl0.5_pw1.5"
-            {...props}
-        />
-    )
+  return (
+    <chip
+      pinLabels={pinLabels}
+      supplierPartNumbers={{
+        jlcpcb: ["C15742"],
+      }}
+      manufacturerPartNumber="STM32F405RGT6"
+      footprint="lqfp64_w10_h10_pl0.5_pw1.5"
+      {...props}
+    />
+  )
 }
--- boards/SparkFun-MicroMod-STM32-Processor/SparkFun-MicroMod-STM32-Processor.circuit.tsx
+++ boards/SparkFun-MicroMod-STM32-Processor/SparkFun-MicroMod-STM32-Processor.circuit.tsx
@@ -6,101 +6,119 @@ import { MicroModEdge } from "./MicroModEdge"
 // https://www.sparkfun.com/products/21326
 
 export default () => {
-    return (
-        <board width={22} height={22}>
-            <STM32F405 name="U1" pcbX={0} pcbY={4} schX={0} schY={0} />
-            <MicroModEdge name="J1" pcbX={0} pcbY={-8} schX={8} schY={0} />
+  return (
+    <board width={22} height={22}>
+      <STM32F405 name="U1" pcbX={0} pcbY={4} schX={0} schY={0} />
+      <MicroModEdge name="J1" pcbX={0} pcbY={-8} schX={8} schY={0} />
 
-            <crystal
-                name="Y1"
-                frequency="12MHz"
-                footprint="0402"
-                loadCapacitance="10pF"
-                pcbX={3}
-                pcbY={6}
-                connections={{
-                    pin1: sel.U1.OSC_IN,
-                    pin2: sel.U1.OSC_OUT,
-                }}
-            />
-            <crystal
-                name="Y2"
-                frequency="32.768kHz"
-                footprint="0402"
-                loadCapacitance="7pF"
-                pcbX={-3}
-                pcbY={6}
-                connections={{
-                    pin1: sel.U1.OSC32_IN,
-                    pin2: sel.U1.OSC32_OUT,
-                }}
-            />
+      <crystal
+        name="Y1"
+        frequency="12MHz"
+        footprint="0402"
+        loadCapacitance="10pF"
+        pcbX={3}
+        pcbY={6}
+        connections={{
+          pin1: sel.U1.OSC_IN,
+          pin2: sel.U1.OSC_OUT,
+        }}
+      />
+      <crystal
+        name="Y2"
+        frequency="32.768kHz"
+        footprint="0402"
+        loadCapacitance="7pF"
+        pcbX={-3}
+        pcbY={6}
+        connections={{
+          pin1: sel.U1.OSC32_IN,
+          pin2: sel.U1.OSC32_OUT,
+        }}
+      />
 
-            <capacitor
-                name="C1"
-                capacitance="2.2uF"
-                footprint="cap0402"
-                pcbX={2}
-                pcbY={2}
-                connections={{ pin1: sel.U1.VCAP1, pin2: sel.net().GND }}
-            />
-            <capacitor
-                name="C2"
-                capacitance="2.2uF"
-                footprint="cap0402"
-                pcbX={-2}
-                pcbY={2}
-                connections={{ pin1: sel.U1.VCAP2, pin2: sel.net().GND }}
-            />
+      <capacitor
+        name="C1"
+        capacitance="2.2uF"
+        footprint="cap0402"
+        pcbX={2}
+        pcbY={2}
+        connections={{ pin1: sel.U1.VCAP1, pin2: sel.net().GND }}
+      />
+      <capacitor
+        name="C2"
+        capacitance="2.2uF"
+        footprint="cap0402"
+        pcbX={-2}
+        pcbY={2}
+        connections={{ pin1: sel.U1.VCAP2, pin2: sel.net().GND }}
+      />
 
-            <chip
-                name="U2"
-                manufacturerPartNumber="W25Q128JVSIQ"
-                footprint="soic8"
-                pcbX={5}
-                pcbY={0}
-                pinLabels={{
-                    pin1: ["CS"],
-                    pin2: ["DO"],
-                    pin3: ["WP"],
-                    pin4: ["GND"],
-                    pin5: ["DI"],
-                    pin6: ["CLK"],
-                    pin7: ["HOLD"],
-                    pin8: ["VCC"],
-                }}
-                connections={{
-                    pin1: sel.U1.NSCS,
-                    pin2: sel.U1.MISO1,
-                    pin5: sel.U1.MOSI1,
-                    pin6: sel.U1.SCK1,
-                    pin4: sel.net().GND,
-                }}
-            />
-            <netlabel net="V3_3" schX={6} schY={2} connectsTo={sel.U2.VCC} anchorSide="bottom" />
+      <chip
+        name="U2"
+        manufacturerPartNumber="W25Q128JVSIQ"
+        footprint="soic8"
+        pcbX={5}
+        pcbY={0}
+        pinLabels={{
+          pin1: ["CS"],
+          pin2: ["DO"],
+          pin3: ["WP"],
+          pin4: ["GND"],
+          pin5: ["DI"],
+          pin6: ["CLK"],
+          pin7: ["HOLD"],
+          pin8: ["VCC"],
+        }}
+        connections={{
+          pin1: sel.U1.NSCS,
+          pin2: sel.U1.MISO1,
+          pin5: sel.U1.MOSI1,
+          pin6: sel.U1.SCK1,
+          pin4: sel.net().GND,
+        }}
+      />
+      <netlabel
+        net="V3_3"
+        schX={6}
+        schY={2}
+        connectsTo={sel.U2.VCC}
+        anchorSide="bottom"
+      />
 
-            <netlabel net="GND" schX={0} schY={-10} connectsTo={sel.J1.GND} anchorSide="top" />
-            <netlabel net="V3_3" schX={1} schY={-10} connectsTo={sel.J1.V3_3} anchorSide="bottom" />
+      <netlabel
+        net="GND"
+        schX={0}
+        schY={-10}
+        connectsTo={sel.J1.GND}
+        anchorSide="top"
+      />
+      <netlabel
+        net="V3_3"
+        schX={1}
+        schY={-10}
+        connectsTo={sel.J1.V3_3}
+        anchorSide="bottom"
+      />
 
-            <resistor
-                name="R1"
-                resistance="10k"
-                footprint="0402"
-                pcbX={-4}
-                pcbY={5}
-                connections={{ pin1: sel.U1.NRST, pin2: sel.J1.RESET }}
-            />
-            <resistor
-                name="R2"
-                resistance="10k"
-                footprint="0402"
-                pcbX={-4}
-                pcbY={3}
-                connections={{ pin1: sel.U1.BOOT0, pin2: sel.net().GND }}
-            />
+      <resistor
+        name="R1"
+        resistance="10k"
+        footprint="0402"
+        pcbX={-4}
+        pcbY={5}
+        connections={{ pin1: sel.U1.NRST, pin2: sel.J1.RESET }}
+      />
+      <resistor
+        name="R2"
+        resistance="10k"
+        footprint="0402"
+        pcbX={-4}
+        pcbY={3}
+        connections={{ pin1: sel.U1.BOOT0, pin2: sel.net().GND }}
+      />
 
-            <silkscreentext text="MicroMod" pcbX={0} pcbY={8} />
-            <silkscreentext text="STM32" pcbX={0} pcbY={7} />
-        </board>
-    )
+      <silkscreentext text="MicroMod" pcbX={0} pcbY={8} />
+      <silkscreentext text="STM32" pcbX={0} pcbY={7} />
+    </board>
+  )
 }
