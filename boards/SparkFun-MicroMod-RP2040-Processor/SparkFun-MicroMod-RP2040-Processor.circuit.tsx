78d166fe2f89d6117b4eac81623677eeaf3b5a80
Muhamad Juwandi <muhamadjuwandi28@gmail.com>
2025-12-30 04:57:40 +0700

    fix(rp2040): use string literals for nets and clean PR scope

--- boards/SparkFun-MicroMod-RP2040-Processor/SparkFun-MicroMod-RP2040-Processor.circuit.tsx
+++ boards/SparkFun-MicroMod-RP2040-Processor/SparkFun-MicroMod-RP2040-Processor.circuit.tsx
@@ -1,10 +1,7 @@
 import { RP2040 } from "./RP2040"
 import { W25Q128 } from "./W25Q128"
 import { MicroModEdge } from "./MicroModEdge"
-import { sel } from "@tscircuit/core"
 import type { ChipProps } from "@tscircuit/props"
-
-const net: any = sel.net
 
 export const SparkFunMicroModRP2040 = () => (
   <board width="22mm" height="22mm">
@@ -18,65 +15,65 @@ export const SparkFunMicroModRP2040 = () => (
       layer="top"
       connections={{
         // Power
-        "2": net.V3V3,
-        "74": net.V3V3,
-        "1": net.GND,
-        "7": net.GND,
-        "33": net.GND,
-        "36": net.GND,
-        "39": net.GND,
-        "45": net.GND,
-        "75": net.GND,
-        "6": net.RESET,
-        "11": net.BOOT,
+        "2": "V3V3",
+        "74": "V3V3",
+        "1": "GND",
+        "7": "GND",
+        "33": "GND",
+        "36": "GND",
+        "39": "GND",
+        "45": "GND",
+        "75": "GND",
+        "6": "RESET",
+        "11": "BOOT",
         // USB (Through Mux)
-        "3": net.USB_DP,
-        "5": net.USB_DN,
-        "9": net.USB_VIN,
+        "3": "USB_DP",
+        "5": "USB_DN",
+        "9": "USB_VIN",
         // UART
-        "17": net.UART_TX1,
-        "19": net.UART_RX1,
-        "22": net.UART_TX2,
-        "20": net.UART_RX2,
+        "17": "UART_TX1",
+        "19": "UART_RX1",
+        "22": "UART_TX2",
+        "20": "UART_RX2",
         // SWD
-        "23": net.SWDIO,
-        "21": net.SWDCK,
+        "23": "SWDIO",
+        "21": "SWDCK",
         // I2C
-        "12": net.I2C_SDA,
-        "14": net.I2C_SCL,
-        "16": net.UART_TX2,
+        "12": "I2C_SDA",
+        "14": "I2C_SCL",
+        "16": "UART_TX2",
         // SPI / G-Pins
-        "71": net.G6,
-        "69": net.G7,
-        "48": net.G4,
-        "73": net.G5,
+        "71": "G6",
+        "69": "G7",
+        "48": "G4",
+        "73": "G5",
         // Audio
-        "50": net.AUD_BCLK,
-        "52": net.AUD_LRCLK,
-        "56": net.AUD_OUT,
-        "54": net.AUD_IN,
-        "58": net.AUD_MCLK,
-        "47": net.AUD_MCLK,
+        "50": "AUD_BCLK",
+        "52": "AUD_LRCLK",
+        "56": "AUD_OUT",
+        "54": "AUD_IN",
+        "58": "AUD_MCLK",
+        "47": "AUD_MCLK",
         // ADC
-        "34": net.ADC0,
-        "38": net.ADC1,
-        "65": net.G9,
-        "49": net.BATT_VIN_DIV3,
+        "34": "ADC0",
+        "38": "ADC1",
+        "65": "G9",
+        "49": "BATT_VIN_DIV3",
         // PWM
-        "32": net.PWM0,
+        "32": "PWM0",
         // SDIO
-        "64": net.SPI_SDI1,
-        "66": net.AUD_IN,
-        "60": net.SPI_SCK1,
-        "62": net.SPI_SDO1,
-        "70": net.UART_RX2,
-        "68": net.AUD_OUT,
+        "64": "SPI_SDI1",
+        "66": "AUD_IN",
+        "60": "SPI_SCK1",
+        "62": "SPI_SDO1",
+        "70": "UART_RX2",
+        "68": "AUD_OUT",
         // G-Pins
-        "40": net.G0,
-        "42": net.G1,
-        "44": net.G2,
-        "46": net.G3,
-        "63": net.G10,
+        "40": "G0",
+        "42": "G1",
+        "44": "G2",
+        "46": "G3",
+        "63": "G10",
       }}
     />
 
@@ -90,66 +87,66 @@ export const SparkFunMicroModRP2040 = () => (
       layer="top"
       connections={{
         // GPIO
-        "2": net.UART_TX1,
-        "3": net.UART_RX1,
-        "4": net.AUD_LRCLK,
-        "5": net.AUD_BCLK,
-        "6": net.I2C_SDA,
-        "7": net.I2C_SCL,
-        "8": net.D0,
-        "9": net.D1,
-        "11": net.UART_TX2,
-        "12": net.UART_RX2,
-        "13": net.AUD_OUT,
-        "14": net.AUD_IN,
-        "15": net.SPI_SDI1,
-        "16": net.PWM0,
-        "17": net.SPI_SCK1,
-        "18": net.SPI_SDO1,
-        "27": net.G0,
-        "28": net.G1,
-        "29": net.G2,
-        "30": net.G3,
-        "31": net.G4,
-        "32": net.G5,
-        "34": net.G6,
-        "35": net.G7,
-        "36": net.AUD_MCLK,
-        "37": net.G10,
-        "38": net.ADC0,
-        "39": net.ADC1,
-        "40": net.G9,
-        "41": net.BATT_VIN_DIV3,
+        "2": "UART_TX1",
+        "3": "UART_RX1",
+        "4": "AUD_LRCLK",
+        "5": "AUD_BCLK",
+        "6": "I2C_SDA",
+        "7": "I2C_SCL",
+        "8": "D0",
+        "9": "D1",
+        "11": "UART_TX2",
+        "12": "UART_RX2",
+        "13": "AUD_OUT",
+        "14": "AUD_IN",
+        "15": "SPI_SDI1",
+        "16": "PWM0",
+        "17": "SPI_SCK1",
+        "18": "SPI_SDO1",
+        "27": "G0",
+        "28": "G1",
+        "29": "G2",
+        "30": "G3",
+        "31": "G4",
+        "32": "G5",
+        "34": "G6",
+        "35": "G7",
+        "36": "AUD_MCLK",
+        "37": "G10",
+        "38": "ADC0",
+        "39": "ADC1",
+        "40": "G9",
+        "41": "BATT_VIN_DIV3",
         // System
-        "26": net.RESET,
-        "19": net.GND,
-        "25": net.SWDIO,
-        "24": net.SWDCK,
-        "20": net.XIN,
-        "21": net.XOUT,
+        "26": "RESET",
+        "19": "GND",
+        "25": "SWDIO",
+        "24": "SWDCK",
+        "20": "XIN",
+        "21": "XOUT",
         // Flash (QSPI)
-        "56": net.QSPI_SS,
-        "52": net.QSPI_SCLK,
-        "53": net.QSPI_SD0,
-        "55": net.QSPI_SD1,
-        "54": net.QSPI_SD2,
-        "51": net.QSPI_SD3,
+        "56": "QSPI_SS",
+        "52": "QSPI_SCLK",
+        "53": "QSPI_SD0",
+        "55": "QSPI_SD1",
+        "54": "QSPI_SD2",
+        "51": "QSPI_SD3",
         // USB - Connects to Mux U2
-        "46": net.RP_DN,
-        "47": net.RP_DP,
+        "46": "RP_DN",
+        "47": "RP_DP",
         // Power
-        "1": net.V3V3,
-        "10": net.V3V3,
-        "22": net.V3V3,
-        "33": net.V3V3,
-        "42": net.V3V3,
-        "49": net.V3V3,
-        "43": net.V3V3,
-        "44": net.V3V3,
-        "48": net.V3V3,
-        "57": net.GND,
-        "23": net.V1V1,
-        "50": net.V1V1,
+        "1": "V3V3",
+        "10": "V3V3",
+        "22": "V3V3",
+        "33": "V3V3",
+        "42": "V3V3",
+        "49": "V3V3",
+        "43": "V3V3",
+        "44": "V3V3",
+        "48": "V3V3",
+        "57": "GND",
+        "23": "V1V1",
+        "50": "V1V1",
       }}
     />
 
@@ -162,14 +159,14 @@ export const SparkFunMicroModRP2040 = () => (
       pcbRotation="180deg"
       layer="bottom"
       connections={{
-        nCS: net.QSPI_SS,
-        CLK: net.QSPI_SCLK,
-        DI: net.QSPI_SD0,
-        DO: net.QSPI_SD1,
-        nWP: net.QSPI_SD2,
-        nHOLD: net.QSPI_SD3,
-        VCC: net.V3V3,
-        GND: net.GND,
+        nCS: "QSPI_SS",
+        CLK: "QSPI_SCLK",
+        DI: "QSPI_SD0",
+        DO: "QSPI_SD1",
+        nWP: "QSPI_SD2",
+        nHOLD: "QSPI_SD3",
+        VCC: "V3V3",
+        GND: "GND",
       }}
     />
 
@@ -195,16 +192,16 @@ export const SparkFunMicroModRP2040 = () => (
         "10": "GND",
       }}
       connections={{
-        "1": net.USB_DP,
-        "2": net.USB_DN, // Host
-        "3": net.USBHOST_DP,
-        "4": net.USBHOST_DN,
-        "5": net.V3V3,
-        "6": net.RP_DN,
-        "7": net.GND,
-        "8": net.RP_DP,
-        "9": net.USB_MUX_SIG, // Select
-        "10": net.GND,
+        "1": "USB_DP",
+        "2": "USB_DN", // Host
+        "3": "USBHOST_DP",
+        "4": "USBHOST_DN",
+        "5": "V3V3",
+        "6": "RP_DN",
+        "7": "GND",
+        "8": "RP_DP",
+        "9": "USB_MUX_SIG", // Select
+        "10": "GND",
       }}
     />
 
@@ -218,7 +215,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbRotation="-90deg"
       layer="top"
       pinLabels={{ "1": "1", "2": "2", "3": "3", "4": "4" }}
-      connections={{ "1": net.XIN, "2": net.GND, "3": net.XOUT, "4": net.GND }}
+      connections={{ "1": "XIN", "2": "GND", "3": "XOUT", "4": "GND" }}
     />
 
     {/* Status LED (D2) - Top */}
@@ -230,7 +227,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={10}
       pcbRotation="180deg"
       layer="top"
-      connections={{ anode: net.LED_ANODE, cathode: net.GND }}
+      connections={{ anode: "LED_ANODE", cathode: "GND" }}
     />
     <resistor
       name="R1"
@@ -240,7 +237,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={10}
       pcbRotation="0deg"
       layer="top"
-      connections={{ pin1: net.G10, pin2: net.LED_ANODE }}
+      connections={{ pin1: "G10", pin2: "LED_ANODE" }}
     />
 
     {/* Decoupling Capacitors & Resistors */}
@@ -254,7 +251,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={-4}
       pcbRotation="180deg"
       layer="bottom"
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
+      connections={{ pin1: "V3V3", pin2: "GND" }}
     />
     <capacitor
       name="C2"
@@ -264,7 +261,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={-5}
       pcbRotation="180deg"
       layer="bottom"
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
+      connections={{ pin1: "V3V3", pin2: "GND" }}
     />
     <capacitor
       name="C3"
@@ -274,7 +271,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={3}
       pcbRotation="180deg"
       layer="bottom"
-      connections={{ pin1: net.V1V1, pin2: net.GND }}
+      connections={{ pin1: "V1V1", pin2: "GND" }}
     />
     <capacitor
       name="C5"
@@ -284,7 +281,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={1}
       pcbRotation="0deg"
       layer="bottom"
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
+      connections={{ pin1: "V3V3", pin2: "GND" }}
     />
     <capacitor
       name="C7"
@@ -294,7 +291,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={-4.3}
       pcbRotation="-90deg"
       layer="bottom"
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
+      connections={{ pin1: "V3V3", pin2: "GND" }}
     />
     <capacitor
       name="C8"
@@ -304,7 +301,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={-2}
       pcbRotation="180deg"
       layer="bottom"
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
+      connections={{ pin1: "V3V3", pin2: "GND" }}
     />
     <capacitor
       name="C9"
@@ -314,7 +311,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={-4.5}
       pcbRotation="-90deg"
       layer="bottom"
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
+      connections={{ pin1: "V3V3", pin2: "GND" }}
     />
     <capacitor
       name="C10"
@@ -324,7 +321,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={2}
       pcbRotation="180deg"
       layer="bottom"
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
+      connections={{ pin1: "V3V3", pin2: "GND" }}
     />
     <capacitor
       name="C12"
@@ -334,7 +331,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={-1}
       pcbRotation="180deg"
       layer="bottom"
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
+      connections={{ pin1: "V3V3", pin2: "GND" }}
     />
     <capacitor
       name="C14"
@@ -344,7 +341,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={0}
       pcbRotation="0deg"
       layer="bottom"
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
+      connections={{ pin1: "V3V3", pin2: "GND" }}
     />
     <capacitor
       name="C15"
@@ -354,7 +351,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={5}
       pcbRotation="-90deg"
       layer="bottom"
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
+      connections={{ pin1: "V3V3", pin2: "GND" }}
     />
     <capacitor
       name="C29"
@@ -364,7 +361,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={5}
       pcbRotation="-90deg"
       layer="bottom"
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
+      connections={{ pin1: "V3V3", pin2: "GND" }}
     />
 
     {/* Resistors - Bottom */}
@@ -376,7 +373,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={-3}
       pcbRotation="180deg"
       layer="bottom"
-      connections={{ pin1: net.ADC_VDD, pin2: net.V3V3 }}
+      connections={{ pin1: "ADC_VDD", pin2: "V3V3" }}
     />
     <resistor
       name="R4"
@@ -386,7 +383,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={4}
       pcbRotation="90deg"
       layer="bottom"
-      connections={{ pin1: net.QSPI_SS, pin2: net.V3V3 }}
+      connections={{ pin1: "QSPI_SS", pin2: "V3V3" }}
     />
     <resistor
       name="R5"
@@ -396,7 +393,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={3}
       pcbRotation="180deg"
       layer="bottom"
-      connections={{ pin1: net.GND, pin2: net.USB_MUX_SIG }}
+      connections={{ pin1: "GND", pin2: "USB_MUX_SIG" }}
     />
     <resistor
       name="R9"
@@ -406,7 +403,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={4}
       pcbRotation="90deg"
       layer="bottom"
-      connections={{ pin1: net.QSPI_SS, pin2: net.BOOT }}
+      connections={{ pin1: "QSPI_SS", pin2: "BOOT" }}
     />
     <resistor
       name="R10"
@@ -416,7 +413,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={1}
       pcbRotation="0deg"
       layer="bottom"
-      connections={{ pin1: net.RP_DP, pin2: net.USB_DP }}
+      connections={{ pin1: "RP_DP", pin2: "USB_DP" }}
     />
     <resistor
       name="R11"
@@ -426,7 +423,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={0}
       pcbRotation="0deg"
       layer="bottom"
-      connections={{ pin1: net.RP_DN, pin2: net.USB_DN }}
+      connections={{ pin1: "RP_DN", pin2: "USB_DN" }}
     />
     <resistor
       name="R18"
@@ -436,7 +433,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={3}
       pcbRotation="0deg"
       layer="bottom"
-      connections={{ pin1: net.USB_VIN, pin2: net.USB_MUX_SIG }}
+      connections={{ pin1: "USB_VIN", pin2: "USB_MUX_SIG" }}
     />
 
     {/* Top Layer Passives */}
@@ -448,7 +445,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={6}
       pcbRotation="90deg"
       layer="top"
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
+      connections={{ pin1: "V3V3", pin2: "GND" }}
     />
     <capacitor
       name="C6"
@@ -458,7 +455,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={6}
       pcbRotation="90deg"
       layer="top"
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
+      connections={{ pin1: "V3V3", pin2: "GND" }}
     />
     <capacitor
       name="C11"
@@ -468,7 +465,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={-1}
       pcbRotation="0deg"
       layer="top"
-      connections={{ pin1: net.XIN, pin2: net.GND }}
+      connections={{ pin1: "XIN", pin2: "GND" }}
     />
     <capacitor
       name="C13"
@@ -478,7 +475,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={3}
       pcbRotation="0deg"
       layer="top"
-      connections={{ pin1: net.XOUT, pin2: net.GND }}
+      connections={{ pin1: "XOUT", pin2: "GND" }}
     />
     <resistor
       name="R8"
@@ -488,7 +485,7 @@ export const SparkFunMicroModRP2040 = () => (
       pcbY={1}
       pcbRotation="90deg"
       layer="top"
-      connections={{ pin1: net.XIN, pin2: net.XOUT }}
+      connections={{ pin1: "XIN", pin2: "XOUT" }}
     />
   </board>
 )
