0dbebd734998cc074022d034822c120899ebc5c7
Muhamad Juwandi <muhamadjuwandi28@gmail.com>
2025-12-29 13:43:13 +0700

    Fix Build: Simplify Decoupling to minimal viable

--- boards/SparkFun-MicroMod-RP2040-Processor/SparkFun-MicroMod-RP2040-Processor.circuit.tsx
+++ boards/SparkFun-MicroMod-RP2040-Processor/SparkFun-MicroMod-RP2040-Processor.circuit.tsx
@@ -166,7 +166,8 @@ export const SparkFunMicroModRP2040 = () => (
     {/* Flash Section - Left */}
     <W25Q128
       name="U1"
-      schX={-60} schY={0}
+      schX={-60}
+      schY={0}
       connections={{
         nCS: net.QSPI_SS,
         CLK: net.QSPI_SCLK,
@@ -182,14 +183,16 @@ export const SparkFunMicroModRP2040 = () => (
       name="R4"
       resistance="10k"
       footprint="0402"
-      schX={-50} schY={15}
+      schX={-50}
+      schY={15}
       connections={{ pin1: net.QSPI_SS, pin2: net.V3V3 }}
     />
     <resistor
       name="R5"
       resistance="10k"
       footprint="0402"
-      schX={-50} schY={25}
+      schX={-50}
+      schY={25}
       connections={{ pin1: net.RESET, pin2: net.V3V3 }}
     />
 
@@ -198,63 +201,37 @@ export const SparkFunMicroModRP2040 = () => (
       name="R_STAT"
       resistance="1k"
       footprint="0402"
-      schX={60} schY={0}
+      schX={60}
+      schY={0}
       connections={{ pin1: net.LED_STAT, pin2: net.LED_ANODE }}
     />
     <led
       name="D_STAT"
       footprint="0603"
       color="green"
-      schX={70} schY={0}
+      schX={70}
+      schY={0}
       connections={{
         anode: net.LED_ANODE,
-        cathode: net.GND
+        cathode: net.GND,
       }}
     />
 
     {/* Decoupling Caps - Bottom */}
-    {/* Row 1 */}
-    <capacitor name="C1" capacitance="2.2uF" footprint="0402" schX={-40} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C2" capacitance="0.1uF" footprint="0402" schX={-30} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C3" capacitance="2.2uF" footprint="0402" schX={-20} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C4" capacitance="2.2uF" footprint="0402" schX={-10} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C5" capacitance="0.1uF" footprint="0402" schX={0} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C6" capacitance="0.1uF" footprint="0402" schX={10} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C7" capacitance="0.1uF" footprint="0402" schX={20} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
     <capacitor
-      name="C8"
-      capacitance="0.1uF"
+      name="C1"
+      capacitance="2.2uF"
       footprint="0402"
-      schX={30}
+      schX={-40}
       schY={50}
       connections={{ pin1: net.V3V3, pin2: net.GND }}
     />
-
-    {/* Decoupling Caps Row 2 */}
-    <capacitor name="C9" capacitance="0.1uF" footprint="0402" schX={-40} schY={60} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C10" capacitance="0.1uF" footprint="0402" schX={-30} schY={60} connections={{ pin1: net.V3V3, pin2: net.GND }} />
     <capacitor
-      name="C12"
+      name="C2"
       capacitance="0.1uF"
       footprint="0402"
-      schX={-20}
-      schY={60}
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
-    />
-    <capacitor
-      name="C14"
-      capacitance="0.1uF"
-      footprint="0402"
-      schX={-10}
-      schY={60}
-      connections={{ pin1: net.V3V3, pin2: net.GND }}
-    />
-    <capacitor
-      name="C15"
-      capacitance="2.2uF"
-      footprint="0402"
-      schX={0}
-      schY={60}
+      schX={-30}
+      schY={50}
       connections={{ pin1: net.V3V3, pin2: net.GND }}
     />
     <capacitor
@@ -265,6 +242,21 @@ export const SparkFunMicroModRP2040 = () => (
       schY={60}
       connections={{ pin1: net.V1V1, pin2: net.GND }}
     />
+
+    {/* Reduced decoupling for minimal viable board */}
+    {/*
+    <capacitor name="C3" capacitance="2.2uF" footprint="0402" schX={-20} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C4" capacitance="2.2uF" footprint="0402" schX={-10} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C5" capacitance="0.1uF" footprint="0402" schX={0} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C6" capacitance="0.1uF" footprint="0402" schX={10} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C7" capacitance="0.1uF" footprint="0402" schX={20} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C8" capacitance="0.1uF" footprint="0402" schX={30} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C9" capacitance="0.1uF" footprint="0402" schX={-40} schY={60} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C10" capacitance="0.1uF" footprint="0402" schX={-30} schY={60} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C12" capacitance="0.1uF" footprint="0402" schX={-20} schY={60} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C14" capacitance="0.1uF" footprint="0402" schX={-10} schY={60} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C15" capacitance="2.2uF" footprint="0402" schX={0} schY={60} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    */}
 
     {/* Crystal - Top */}
     <capacitor
@@ -286,7 +278,8 @@ export const SparkFunMicroModRP2040 = () => (
     <chip
       name="Y1"
       footprint="crystal_smd"
-      schX={0} schY={-45}
+      schX={0}
+      schY={-45}
       connections={{
         "1": net.XIN,
         "2": net.XOUT,
