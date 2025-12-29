d8fc5aadda24e480ed343c1822250208e5667711
Muhamad Juwandi <muhamadjuwandi28@gmail.com>
2025-12-29 11:11:33 +0700

    Fix CI: Update Format and Snapshots

--- boards/SparkFun-MicroMod-RP2040-Processor/SparkFun-MicroMod-RP2040-Processor.circuit.tsx
+++ boards/SparkFun-MicroMod-RP2040-Processor/SparkFun-MicroMod-RP2040-Processor.circuit.tsx
@@ -9,8 +9,8 @@ export const SparkFunMicroModRP2040 = () => (
   <board width="22mm" height="22mm">
     <MicroModEdge
       name="J1"
-      schX={40}
-      schY={0}
+      schX={0}
+      schY={-80}
       pcbX={0}
       pcbY={-9.5}
       connections={{
@@ -163,9 +163,10 @@ export const SparkFunMicroModRP2040 = () => (
       }}
     />
 
+    {/* Flash Section - Left */}
     <W25Q128
       name="U1"
-      schX={-40} schY={0}
+      schX={-60} schY={0}
       connections={{
         nCS: net.QSPI_SS,
         CLK: net.QSPI_SCLK,
@@ -181,61 +182,111 @@ export const SparkFunMicroModRP2040 = () => (
       name="R4"
       resistance="10k"
       footprint="0402"
-      schX={-30} schY={10}
+      schX={-50} schY={15}
       connections={{ pin1: net.QSPI_SS, pin2: net.V3V3 }}
     />
     <resistor
       name="R5"
       resistance="10k"
       footprint="0402"
-      schX={-30} schY={15}
+      schX={-50} schY={25}
       connections={{ pin1: net.RESET, pin2: net.V3V3 }}
     />
 
-    {/* Status LED */}
+    {/* Status LED - Right */}
     <resistor
       name="R_STAT"
       resistance="1k"
       footprint="0402"
-      schX={20} schY={-30}
+      schX={60} schY={0}
       connections={{ pin1: net.LED_STAT, pin2: net.LED_ANODE }}
     />
     <led
       name="D_STAT"
       footprint="0603"
       color="green"
-      schX={25} schY={-30}
+      schX={70} schY={0}
       connections={{
         anode: net.LED_ANODE,
         cathode: net.GND
       }}
     />
 
-    {/* Decoupling Caps Row 1 */}
-    <capacitor name="C1" capacitance="2.2uF" footprint="0402" schX={-15} schY={30} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C2" capacitance="0.1uF" footprint="0402" schX={-10} schY={30} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C3" capacitance="2.2uF" footprint="0402" schX={-5} schY={30} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C4" capacitance="2.2uF" footprint="0402" schX={0} schY={30} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C5" capacitance="0.1uF" footprint="0402" schX={5} schY={30} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C6" capacitance="0.1uF" footprint="0402" schX={10} schY={30} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C7" capacitance="0.1uF" footprint="0402" schX={15} schY={30} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    {/* Decoupling Caps - Bottom */}
+    {/* Row 1 */}
+    <capacitor name="C1" capacitance="2.2uF" footprint="0402" schX={-40} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C2" capacitance="0.1uF" footprint="0402" schX={-30} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C3" capacitance="2.2uF" footprint="0402" schX={-20} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C4" capacitance="2.2uF" footprint="0402" schX={-10} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C5" capacitance="0.1uF" footprint="0402" schX={0} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C6" capacitance="0.1uF" footprint="0402" schX={10} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C7" capacitance="0.1uF" footprint="0402" schX={20} schY={50} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor
+      name="C8"
+      capacitance="0.1uF"
+      footprint="0402"
+      schX={30}
+      schY={50}
+      connections={{ pin1: net.V3V3, pin2: net.GND }}
+    />
 
     {/* Decoupling Caps Row 2 */}
-    <capacitor name="C8" capacitance="0.1uF" footprint="0402" schX={-15} schY={40} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C9" capacitance="0.1uF" footprint="0402" schX={-10} schY={40} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C10" capacitance="0.1uF" footprint="0402" schX={-5} schY={40} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C12" capacitance="0.1uF" footprint="0402" schX={0} schY={40} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C14" capacitance="0.1uF" footprint="0402" schX={5} schY={40} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C15" capacitance="2.2uF" footprint="0402" schX={10} schY={40} connections={{ pin1: net.V3V3, pin2: net.GND }} />
-    <capacitor name="C_Reg" capacitance="2.2uF" footprint="0402" schX={15} schY={40} connections={{ pin1: net.V1V1, pin2: net.GND }} />
+    <capacitor name="C9" capacitance="0.1uF" footprint="0402" schX={-40} schY={60} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor name="C10" capacitance="0.1uF" footprint="0402" schX={-30} schY={60} connections={{ pin1: net.V3V3, pin2: net.GND }} />
+    <capacitor
+      name="C12"
+      capacitance="0.1uF"
+      footprint="0402"
+      schX={-20}
+      schY={60}
+      connections={{ pin1: net.V3V3, pin2: net.GND }}
+    />
+    <capacitor
+      name="C14"
+      capacitance="0.1uF"
+      footprint="0402"
+      schX={-10}
+      schY={60}
+      connections={{ pin1: net.V3V3, pin2: net.GND }}
+    />
+    <capacitor
+      name="C15"
+      capacitance="2.2uF"
+      footprint="0402"
+      schX={0}
+      schY={60}
+      connections={{ pin1: net.V3V3, pin2: net.GND }}
+    />
+    <capacitor
+      name="C_Reg"
+      capacitance="2.2uF"
+      footprint="0402"
+      schX={10}
+      schY={60}
+      connections={{ pin1: net.V1V1, pin2: net.GND }}
+    />
 
-    {/* Crystal */}
-    <capacitor name="C11" capacitance="15pF" footprint="0402" schX={-25} schY={-30} connections={{ pin1: net.XIN, pin2: net.GND }} />
-    <capacitor name="C13" capacitance="15pF" footprint="0402" schX={-25} schY={-35} connections={{ pin1: net.XOUT, pin2: net.GND }} />
+    {/* Crystal - Top */}
+    <capacitor
+      name="C11"
+      capacitance="15pF"
+      footprint="0402"
+      schX={-10}
+      schY={-40}
+      connections={{ pin1: net.XIN, pin2: net.GND }}
+    />
+    <capacitor
+      name="C13"
+      capacitance="15pF"
+      footprint="0402"
+      schX={10}
+      schY={-40}
+      connections={{ pin1: net.XOUT, pin2: net.GND }}
+    />
     <chip
       name="Y1"
       footprint="crystal_smd"
-      schX={-20} schY={-32}
+      schX={0} schY={-45}
       connections={{
         "1": net.XIN,
         "2": net.XOUT,
