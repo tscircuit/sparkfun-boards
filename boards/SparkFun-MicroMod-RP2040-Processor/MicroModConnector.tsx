4241843f99e74505c2e674bdf42c032699e82462
natinew77-creator <nati.new77@gmail.com>
2026-01-09 10:39:04 -0500

    fix: Resolve CI build errors (imports and connections syntax)

--- boards/SparkFun-MicroMod-RP2040-Processor/MicroModConnector.tsx
+++ boards/SparkFun-MicroMod-RP2040-Processor/MicroModConnector.tsx
@@ -1,47 +1,79 @@
 import { Chip } from "tscircuit"
 
 export const MicroModConnector = (props: any) => (
-    <chip
-        {...props}
-        name={props.name || "J1"}
-        footprint="m2_micromod_connector" // Placeholder for M.2 MicroMod Connector
-        pinLabels={{
-            // Power / Control
-            pin1: ["GND"], pin2: ["3.3V"],
-            pin3: ["USB_D+"], pin4: ["3.3V_EN"],
-            pin5: ["USB_D-"], pin6: ["RESET#"],
-            pin7: ["GND"], pin8: ["GND"],
-            pin9: ["USB_VIN"], pin10: ["D0"],
-            pin11: ["BOOT"], pin12: ["I2C_SDA"],
-            pin13: ["GND"], pin14: ["I2C_SCL"],
-            pin15: ["BATT_VIN/3"], pin16: ["I2C_INT#"],
-            pin17: ["BATT_VIN"], pin18: ["D1"],
-            pin19: ["GND"], pin20: ["RX1"],
-            pin21: ["TX1"], pin22: ["RX2"],
-            pin23: ["CTS1"], pin24: ["TX2"],
-            pin25: ["RTS1"], pin26: ["GND"],
-            pin27: ["PWM0"], pin28: ["GND"],
-            pin29: ["PWM1"], pin30: ["GND"],
-            pin31: ["SPI_SCK"], pin32: ["GND"],
-            pin33: ["SPI_CIPO"], pin34: ["A0"],
-            pin35: ["SPI_COPI"], pin36: ["GND"],
-            pin37: ["SPI_CS#"], pin38: ["A1"],
-            pin39: ["G0"], pin40: ["GND"],
-            pin41: ["G1"], pin42: ["G4"],
-            pin43: ["G2"], pin44: ["G5"],
-            pin45: ["G3"], pin46: ["G6"],
-            pin47: ["GND"], pin48: ["G7"],
-            pin49: ["GND"], pin50: ["G8"],
-            pin51: ["G9"], pin52: ["G11/SWO"],
-            pin53: ["G10"], pin54: ["SWD_CLK"],
-            pin55: ["SWD_IO"], // SWD_IO from our netlist
-            // Key E
-            // 56-65 are typically Key
-            pin66: ["SDIO_DATA2"], pin67: ["SDIO_DATA3"],
-            pin68: ["SDIO_DATA1"], pin69: ["SDIO_CMD"],
-            pin70: ["SDIO_DATA0"], pin71: ["SDIO_CLK"],
-            pin72: ["RTC_3V"], pin73: ["3.3V"],
-            pin74: ["3.3V"], pin75: ["GND"]
-        }}
-    />
+  <chip
+    {...props}
+    name={props.name || "J1"}
+    footprint="m2_micromod_connector" // Placeholder for M.2 MicroMod Connector
+    pinLabels={{
+      // Power / Control
+      pin1: ["GND"],
+      pin2: ["3.3V"],
+      pin3: ["USB_D+"],
+      pin4: ["3.3V_EN"],
+      pin5: ["USB_D-"],
+      pin6: ["RESET#"],
+      pin7: ["GND"],
+      pin8: ["GND"],
+      pin9: ["USB_VIN"],
+      pin10: ["D0"],
+      pin11: ["BOOT"],
+      pin12: ["I2C_SDA"],
+      pin13: ["GND"],
+      pin14: ["I2C_SCL"],
+      pin15: ["BATT_VIN/3"],
+      pin16: ["I2C_INT#"],
+      pin17: ["BATT_VIN"],
+      pin18: ["D1"],
+      pin19: ["GND"],
+      pin20: ["RX1"],
+      pin21: ["TX1"],
+      pin22: ["RX2"],
+      pin23: ["CTS1"],
+      pin24: ["TX2"],
+      pin25: ["RTS1"],
+      pin26: ["GND"],
+      pin27: ["PWM0"],
+      pin28: ["GND"],
+      pin29: ["PWM1"],
+      pin30: ["GND"],
+      pin31: ["SPI_SCK"],
+      pin32: ["GND"],
+      pin33: ["SPI_CIPO"],
+      pin34: ["A0"],
+      pin35: ["SPI_COPI"],
+      pin36: ["GND"],
+      pin37: ["SPI_CS#"],
+      pin38: ["A1"],
+      pin39: ["G0"],
+      pin40: ["GND"],
+      pin41: ["G1"],
+      pin42: ["G4"],
+      pin43: ["G2"],
+      pin44: ["G5"],
+      pin45: ["G3"],
+      pin46: ["G6"],
+      pin47: ["GND"],
+      pin48: ["G7"],
+      pin49: ["GND"],
+      pin50: ["G8"],
+      pin51: ["G9"],
+      pin52: ["G11/SWO"],
+      pin53: ["G10"],
+      pin54: ["SWD_CLK"],
+      pin55: ["SWD_IO"], // SWD_IO from our netlist
+      // Key E
+      // 56-65 are typically Key
+      pin66: ["SDIO_DATA2"],
+      pin67: ["SDIO_DATA3"],
+      pin68: ["SDIO_DATA1"],
+      pin69: ["SDIO_CMD"],
+      pin70: ["SDIO_DATA0"],
+      pin71: ["SDIO_CLK"],
+      pin72: ["RTC_3V"],
+      pin73: ["3.3V"],
+      pin74: ["3.3V"],
+      pin75: ["GND"],
+    }}
+  />
 )
--- boards/SparkFun-MicroMod-RP2040-Processor/SparkFun-MicroMod-RP2040-Processor.circuit.tsx
+++ boards/SparkFun-MicroMod-RP2040-Processor/SparkFun-MicroMod-RP2040-Processor.circuit.tsx
@@ -1,207 +1,232 @@
 import { RP2040 } from "./RP2040"
 import { MicroModConnector } from "./MicroModConnector"
+import { sel } from "tscircuit"
 
-import { Resistor, Capacitor, Led, Chip } from "@tscircuit/parts"
+const nets = sel.net as any
 
 export const SparkFunMicroModRP2040 = () => (
-    <board width="22mm" height="22mm">
+  <board width="22mm" height="22mm">
 
-        <RP2040 name="U3"
-            schX={0} schY={0}
-            footprint="qfn56_7x7"
-        />
+    <RP2040 name="U3"
+      schX={0} schY={0}
+      footprint="qfn56_7x7"
+      connections={{
+        pin1: nets.V3_3, // IOVDD
+        pin2: nets.TX1, // GPIO0 (UART TX1)
+        pin3: nets.RX1, // GPIO1 (UART RX1)
+        pin4: nets.CTS1, // GPIO2
+        pin5: nets.RTS1, // GPIO3
+        pin6: nets.I2C_SDA, // GPIO4
+        pin7: nets.I2C_SCL, // GPIO5
+        pin8: nets.D0, // GPIO6
+        pin9: nets.D1, // GPIO7
+        pin10: nets.V3_3, // IOVDD
+        pin11: nets.I2C_INT, // GPIO8
+        pin12: nets.RX2, // GPIO9
+        pin13: nets.SDIO_DATA2, // GPIO10
+        pin14: nets.SDIO_DATA1, // GPIO11
+        pin15: nets.SDIO_DATA0, // GPIO12
+        pin16: nets.PWM0, // GPIO13
+        pin17: nets.SDIO_CLK, // GPIO14
+        pin18: nets.SDIO_CMD, // GPIO15
+        pin19: nets.GND, // TESTEN (Grounded)
+        pin20: nets.XIN, // XIN
+        pin21: nets.XOUT, // XOUT
+        pin22: nets.V3_3, // IOVDD
+        pin23: nets.V1_1, // DVDD
+        pin24: nets.SWCLK, // SWCLK
+        pin25: nets.SWDIO, // SWDIO
+        pin26: nets.RESET, // RUN
+        pin27: nets.G0, // GPIO16
+        pin28: nets.G1, // GPIO17
+        pin29: nets.G2, // GPIO18
+        pin30: nets.G3, // GPIO19
+        pin31: nets.SPI_CIPO, // GPIO20
+        pin32: nets.SPI_CS, // GPIO21
+        pin33: nets.V3_3, // IOVDD
+        pin34: nets.SPI_SCK, // GPIO22
+        pin35: nets.SPI_COPI, // GPIO23
+        pin36: nets.PWM1, // GPIO24
+        pin37: nets.LED_SIG, // GPIO25 (LED)
+        pin38: nets.A0, // GPIO26_ADC0
+        pin39: nets.A1, // GPIO27_ADC1
+        pin40: nets.G9, // GPIO28_ADC2
+        pin41: nets.BATT_VIN_3, // GPIO29_ADC3
+        pin42: nets.V3_3, // IOVDD
+        pin43: nets.V3_3, // ADC_AVDD
+        pin44: nets.V3_3, // VREG_VIN
+        pin45: nets.V1_1, // VREG_VOUT
+        pin46: nets.RP_D_N, // USB_DM
+        pin47: nets.RP_D_P, // USB_DP
+        pin48: nets.V3_3, // USB_VDD
+        pin49: nets.V3_3, // IOVDD
+        pin50: nets.V1_1, // DVDD
+        pin51: nets.QSPI_SD3,
+        pin52: nets.QSPI_SCLK,
+        pin53: nets.QSPI_SD0,
+        pin54: nets.QSPI_SD2,
+        pin55: nets.QSPI_SD1,
+        pin56: nets.QSPI_SS,
+        pin57: nets.GND, // E-Pad
+      }}
+    />
 
-        <MicroModConnector name="J1"
-            schX={-25} schY={0}
-        />
+    <MicroModConnector name="J1"
+      schX={-25} schY={0}
+      connections={{
+        pin1: nets.GND,
+        pin2: nets.V3_3,
+        pin3: nets.USB_D_P, // USB_D+
+        pin5: nets.USB_D_N, // USB_D-
+        pin6: nets.RESET,
+        pin7: nets.GND,
+        pin8: nets.GND,
+        pin9: nets.USB_VIN,
+        pin10: nets.D0,
+        pin11: nets.BOOT,
+        pin12: nets.I2C_SDA,
+        pin13: nets.GND,
+        pin14: nets.I2C_SCL,
+        pin15: nets.BATT_VIN_3,
+        pin16: nets.I2C_INT,
+        pin18: nets.D1,
+        pin19: nets.GND,
+        pin20: nets.RX1,
+        pin21: nets.TX1,
+        pin22: nets.RX2,
+        pin23: nets.CTS1,
+        pin24: nets.I2C_INT,
+        pin25: nets.RTS1,
+        pin26: nets.GND,
+        pin27: nets.PWM0,
+        pin28: nets.GND,
+        pin29: nets.PWM1,
+        pin30: nets.GND,
+        pin31: nets.SPI_SCK,
+        pin32: nets.GND,
+        pin33: nets.SPI_CIPO,
+        pin34: nets.A0,
+        pin35: nets.SPI_COPI,
+        pin36: nets.GND,
+        pin37: nets.SPI_CS,
+        pin38: nets.A1,
+        pin39: nets.G0,
+        pin40: nets.GND,
+        pin41: nets.G1,
+        pin42: nets.SPI_CIPO,
+        pin43: nets.G2,
+        pin44: nets.SPI_CS,
+        pin45: nets.G3,
+        pin46: nets.SPI_SCK,
+        pin47: nets.GND,
+        pin48: nets.SPI_COPI,
+        pin49: nets.GND,
+        pin50: nets.I2C_INT,
+        pin51: nets.G9,
+        // pin52: G11/SWO
+        pin53: nets.LED_SIG, // G10
+        pin54: nets.SWCLK,
+        pin55: nets.SWDIO,
 
-        {/* Flash Memory */}
-        <chip name="U1" manufacturerPartNumber="W25Q128" footprint="soic8"
-            pinLabels={{
-                pin1: ["CS#"], pin2: ["DO/IO1"], pin3: ["WP#/IO2"], pin4: ["GND"],
-                pin5: ["DI/IO0"], pin6: ["CLK"], pin7: ["HOLD#/IO3"], pin8: ["VCC"]
-            }}
-            schX={15} schY={10}
-        />
+        pin66: nets.SDIO_DATA2,
+        pin67: nets.SPI_CS, // SDIO_DATA3 (CS1)
+        pin68: nets.SDIO_DATA1,
+        pin69: nets.SDIO_CMD,
+        pin70: nets.SDIO_DATA0,
+        pin71: nets.SDIO_CLK,
 
-        {/* Crystal 12MHz */}
-        <component name="Y1" pcbX={5} pcbY={-5} schX={10} schY={-10}>
-            <footprint>
-                <smtpad shape="rect" width="1mm" height="1mm" layer="top" pcbX={-1} pcbY={0} portHints={["pin1"]} />
-                <smtpad shape="rect" width="1mm" height="1mm" layer="top" pcbX={1} pcbY={0} portHints={["pin2"]} />
-            </footprint>
-        </component>
+        pin72: nets.V3_3, // RTC_3V
+        pin73: nets.V3_3,
+        pin74: nets.V3_3,
+        pin75: nets.GND
+      }}
+    />
 
-        {/* USB Mux - FSUSB30MUX */}
-        <chip name="U2" manufacturerPartNumber="FSUSB30MUX" footprint="msop10"
-            pinLabels={{
-                pin1: ["VCC"], pin2: ["S"], pin3: ["OE#"], pin4: ["HSD1-"], pin5: ["HSD1+"],
-                pin6: ["HSD2+"], pin7: ["HSD2-"], pin8: ["GND"], pin9: ["D-"], pin10: ["D+"]
-            }}
-            schX={-15} schY={15}
-        />
+    {/* Flash Memory - W25Q128 */}
+    <chip name="U1" manufacturerPartNumber="W25Q128" footprint="soic8"
+      pinLabels={{
+        pin1: ["CS#"], pin2: ["DO/IO1"], pin3: ["WP#/IO2"], pin4: ["GND"],
+        pin5: ["DI/IO0"], pin6: ["CLK"], pin7: ["HOLD#/IO3"], pin8: ["VCC"]
+      }}
+      schX={15} schY={10}
+      connections={{
+        pin1: nets.QSPI_SS,
+        pin2: nets.QSPI_SD1,
+        pin3: nets.QSPI_SD2,
+        pin4: nets.GND,
+        pin5: nets.QSPI_SD0,
+        pin6: nets.QSPI_SCLK,
+        pin7: nets.QSPI_SD3,
+        pin8: nets.V3_3
+      }}
+    />
 
-        {/* LED D2 (Blue) - Status LED */}
-        <Led name="D2" color="blue" footprint="0603" schX={10} schY={15} />
-        <Resistor name="R1" resistance="1k" footprint="0402" schX={5} schY={15} />
-
-        {/* USB Resistors */}
-        <Resistor name="R10" resistance="27" footprint="0402" schX={-5} schY={5} />
-        <Resistor name="R11" resistance="27" footprint="0402" schX={-5} schY={7} />
-
-        {/* USB VBUS Divider */}
-        <Resistor name="R18" resistance="1k" footprint="0402" schX={-20} schY={18} />
-        <Resistor name="R5" resistance="10k" footprint="0402" schX={-20} schY={20} />
-
-        {/* Caps (Simplification: generic decoupling) */}
-        <Capacitor name="C3" capacitance="2.2uF" footprint="0402" schX={0} schY={-15} />
-        <Capacitor name="C4" capacitance="2.2uF" footprint="0402" schX={2} schY={-15} />
-
-        {/* Power Connections */}
-        <net name="3.3V">
-            <port port="J1.pin2" />
-            <port port="J1.pin73" />
-            <port port="J1.pin74" />
-            <port port="U3.IOVDD" />
-            <port port="U3.DVDD" />
-            <port port="U3.ADC_AVDD" />
-            <port port="U3.VREG_VIN" /> {/* VIN typically higher, but schematic showed 3.3V net on VREG_VIN? */}
-            <port port="U3.USB_VDD" />
-            <port port="U1.pin8" />
-            <port port="U2.pin1" />
-            <port port="C3.pin1" />
-            <port port="C4.pin1" />
-        </net>
-
-        <net name="GND">
-            <port port="J1.pin1" />
-            <port port="J1.pin7" />
-            <port port="J1.pin75" />
-            <port port="U3.GND" />
-            <port port="U1.pin4" />
-            <port port="U2.pin8" />
-            <port port="U2.pin3" /> {/* OE# grounded always enabled */}
-            <port port="D2.cathode" />
-            <port port="R5.pin1" />
-            <port port="C3.pin2" />
-            <port port="C4.pin2" />
-            {/* Crystal Caps typically ground too, omitted for brevity but Y1 needs load caps */}
-        </net>
-
-        <net name="1.1V">
-            <port port="U3.VREG_VOUT" />
-            {/* Connects to internal core digital supply DVDD? No, U3.DVDD connected to 3.3V or 1.1V? 
-            Check: RP2040 VREG_VOUT is 1.1V out. DVDD is 1.1V IN.
-            Wait, earlier I connected U3.DVDD to 3.3V. 
-            Correction: RP2040 DVDD is 1.1V Input! IOVDD is 3.3V.
-            I must fix this. 
-            Netlist: Supply8 -> 1.1V
-            Let's assume loopback U3.VREG_VOUT -> U3.DVDD 
-         */}
-            <port port="U3.DVDD" />
-        </net>
-
-        {/* Crystal */}
-        <net name="XIN"><port port="U3.XIN" /><port port="Y1.pin1" /></net>
-        <net name="XOUT"><port port="U3.XOUT" /><port port="Y1.pin2" /></net>
-
-        {/* Flash QSPI */}
-        <net name="QSPI_SS"><port port="U3.QSPI_SS" /><port port="U1.pin1" /></net>
-        <net name="QSPI_SCLK"><port port="U3.QSPI_SCLK" /><port port="U1.pin6" /></net>
-        <net name="QSPI_SD0"><port port="U3.QSPI_SD0" /><port port="U1.pin5" /></net>
-        <net name="QSPI_SD1"><port port="U3.QSPI_SD1" /><port port="U1.pin2" /></net>
-        <net name="QSPI_SD2"><port port="U3.QSPI_SD2" /><port port="U1.pin3" /></net>
-        <net name="QSPI_SD3"><port port="U3.QSPI_SD3" /><port port="U1.pin7" /></net>
-
-        {/* USB Logic */}
-        {/* VBUS Detect */}
-        <net name="USB_VIN">
-            <port port="J1.pin9" />
-            <port port="R18.pin1" />
-        </net>
-        <net name="USB_MUX_SIG">
-            <port port="R18.pin2" />
-            <port port="R5.pin2" />
-            <port port="U2.pin2" /> {/* S */}
-        </net>
-
-        {/* USB Data Path */}
-        {/* J1 to Mux */}
-        <net name="USB_D_N"><port port="J1.pin5" /><port port="U2.pin7" />{/* HSD2- */}</net>
-        <net name="USB_D_P"><port port="J1.pin3" /><port port="U2.pin6" />{/* HSD2+ */}</net>
-        {/* USBHost to Mux (if used) */}
-        {/* <net name="USBHOST_D_N"><port port="J1.pin..." /><port port="U2.pin4" /></net> */}
-
-        {/* Mux Common to RP2040 via Resistors */}
-        <net name="RP_D_P_MUX"><port port="U2.pin10" /><port port="R10.pin1" /></net>
-        <net name="RP_D_N_MUX"><port port="U2.pin9" /><port port="R11.pin1" /></net>
-
-        <net name="RP_D_P"><port port="R10.pin2" /><port port="U3.USB_DP" /></net>
-        <net name="RP_D_N"><port port="R11.pin2" /><port port="U3.USB_DM" /></net>
-
-        {/* Status LED */}
-        <net name="LED_SIG">
-            <port port="U3.GPIO25" />
-            <port port="J1.pin53" /> {/* G10 */}
-            <port port="R1.pin2" />
-        </net>
-        <net name="LED_ANODE"><port port="R1.pin1" /><port port="D2.anode" /></net>
+    {/* Crystal 12MHz */}
+    {/* Using 'component' generic */}
+    <component name="Y1" pcbX={5} pcbY={-5} schX={10} schY={-10}>
+      <footprint>
+        <smtpad shape="rect" width="1mm" height="1mm" layer="top" pcbX={-1} pcbY={0} portHints={["pin1"]} />
+        <smtpad shape="rect" width="1mm" height="1mm" layer="top" pcbX={1} pcbY={0} portHints={["pin2"]} />
+      </footprint>
+    </component>
+    <chip name="Y1_Logic"
+      pinLabels={{ pin1: ["XIN"], pin2: ["XOUT"] }}
+      schX={10} schY={-10}
+      connections={{ pin1: nets.XIN, pin2: nets.XOUT }}
+    />
 
 
-        {/* MicroMod GPIOs */}
-        <net name="UART_TX1"><port port="U3.GPIO0" /><port port="J1.TX1" /></net>
-        <net name="UART_RX1"><port port="U3.GPIO1" /><port port="J1.RX1" /></net>
+    {/* USB Mux - FSUSB30MUX */}
+    <chip name="U2" manufacturerPartNumber="FSUSB30MUX" footprint="msop10"
+      pinLabels={{
+        pin1: ["VCC"], pin2: ["S"], pin3: ["OE#"], pin4: ["HSD1-"], pin5: ["HSD1+"],
+        pin6: ["HSD2+"], pin7: ["HSD2-"], pin8: ["GND"], pin9: ["D-"], pin10: ["D+"]
+      }}
+      schX={-15} schY={15}
+      connections={{
+        pin1: nets.V3_3,
+        pin2: nets.USB_MUX_SIG,
+        pin3: nets.GND,
+        // pin4/5 HSD1 (USBHost?) Unused/Unconnected?
+        pin6: nets.USB_D_P, // HSD2+ -> J1
+        pin7: nets.USB_D_N, // HSD2- -> J1
+        pin8: nets.GND,
+        pin9: nets.RP_D_N_MUX, // D- -> RP2040 (via R11)
+        pin10: nets.RP_D_P_MUX // D+ -> RP2040 (via R10)
+      }}
+    />
 
-        <net name="CTS1"><port port="U3.GPIO2" /><port port="J1.CTS1" /></net>
-        <net name="RTS1"><port port="U3.GPIO3" /><port port="J1.RTS1" /></net>
+    {/* LED D2 (Blue) - Status LED */}
+    <led name="D2" color="blue" footprint="0603" schX={10} schY={15}
+      connections={{ anode: nets.LED_ANODE, cathode: nets.GND }}
+    />
+    <resistor name="R1" resistance="1k" footprint="0402" schX={5} schY={15}
+      connections={{ pin1: nets.LED_ANODE, pin2: nets.LED_SIG }}
+    />
 
-        <net name="I2C_SDA"><port port="U3.GPIO4" /><port port="J1.I2C_SDA" /></net>
-        <net name="I2C_SCL"><port port="U3.GPIO5" /><port port="J1.I2C_SCL" /></net>
+    {/* USB Resistors */}
+    <resistor name="R10" resistance="27" footprint="0402" schX={-5} schY={5}
+      connections={{ pin1: nets.RP_D_P_MUX, pin2: nets.RP_D_P }}
+    />
+    <resistor name="R11" resistance="27" footprint="0402" schX={-5} schY={7}
+      connections={{ pin1: nets.RP_D_N_MUX, pin2: nets.RP_D_N }}
+    />
 
-        <net name="D0"><port port="U3.GPIO6" /><port port="J1.D0" /></net>
-        <net name="D1"><port port="U3.GPIO7" /><port port="J1.D1" /></net>
+    {/* USB VBUS Divider */}
+    <resistor name="R18" resistance="1k" footprint="0402" schX={-20} schY={18}
+      connections={{ pin1: nets.USB_VIN, pin2: nets.USB_MUX_SIG }}
+    />
+    <resistor name="R5" resistance="10k" footprint="0402" schX={-20} schY={20}
+      connections={{ pin1: nets.GND, pin2: nets.USB_MUX_SIG }}
+    />
 
-        <net name="I2C_INT"><port port="U3.GPIO8" /><port port="J1.TX2" /></net>
-        <net name="RX2"><port port="U3.GPIO9" /><port port="J1.RX2" /></net>
+    {/* Caps */}
+    <capacitor name="C3" capacitance="2.2uF" footprint="0402" schX={0} schY={-15}
+      connections={{ pin1: nets.V3_3, pin2: nets.GND }}
+    />
+    <capacitor name="C4" capacitance="2.2uF" footprint="0402" schX={2} schY={-15}
+      connections={{ pin1: nets.V3_3, pin2: nets.GND }}
+    />
 
-        <net name="SDIO_DATA2"><port port="U3.GPIO10" /><port port="J1.SDIO_DATA2" /></net>
-        <net name="SDIO_DATA1"><port port="U3.GPIO11" /><port port="J1.SDIO_DATA1" /></net>
-        <net name="SDIO_DATA0"><port port="U3.GPIO12" /><port port="J1.SDIO_DATA0" /></net>
-
-        <net name="PWM0"><port port="U3.GPIO13" /><port port="J1.PWM0" /></net>
-
-        <net name="SDIO_CLK"><port port="U3.GPIO14" /><port port="J1.SDIO_CLK" /></net>
-        <net name="SDIO_CMD"><port port="U3.GPIO15" /><port port="J1.SDIO_CMD" /></net>
-
-        <net name="G0"><port port="U3.GPIO16" /><port port="J1.G0" /></net>
-        <net name="G1"><port port="U3.GPIO17" /><port port="J1.G1" /></net>
-        <net name="G2"><port port="U3.GPIO18" /><port port="J1.G2" /></net>
-        <net name="G3"><port port="U3.GPIO19" /><port port="J1.G3" /></net>
-
-        <net name="SPI_CIPO"><port port="U3.GPIO20" /><port port="J1.SPI_CIPO" /></net>
-        <net name="SPI_CS"><port port="U3.GPIO21" /><port port="J1.SPI_CS#" /></net>
-        <net name="SPI_SCK"><port port="U3.GPIO22" /><port port="J1.SPI_SCK" /></net>
-        <net name="SPI_COPI"><port port="U3.GPIO23" /><port port="J1.SPI_COPI" /></net>
-
-        <net name="PWM1"><port port="U3.GPIO24" /><port port="J1.PWM1" /></net>
-
-        <net name="A0"><port port="U3.GPIO26_ADC0" /><port port="J1.A0" /></net>
-        <net name="A1"><port port="U3.GPIO27_ADC1" /><port port="J1.A1" /></net>
-        <net name="G9"><port port="U3.GPIO28_ADC2" /><port port="J1.G9" /></net>
-        <net name="BATT_VIN_3"><port port="U3.GPIO29_ADC3" /><port port="J1.BATT_VIN/3" /></net>
-
-        <net name="SWDIO"><port port="U3.SWDIO" /><port port="J1.SWD_IO" /></net>
-        <net name="SWCLK"><port port="U3.SWCLK" /><port port="J1.SWD_CLK" /></net>
-        <net name="RESET"><port port="U3.RUN" /><port port="J1.RESET#" /></net>
-        <net name="BOOT"><port port="J1.BOOT" /></net> {/* BOOT usually pulled down? Not connected to RP2040 directly... wait, QSPI_SS? No. 
-        Flash has to be handled. RP2040 boot mode via QSPI_SS usually.
-        If J1.BOOT is asserted, how does it affect?
-        Schematic check: `Net: BOOT` -> `J1.!BOOT!`. Is it connected to `U1.CS`?
-        Usually BOOT button pulls QSPI_CS low (or similar) on power up.
-        If not connected in netlist, maybe it's unused or I missed it. I'll leave as stub.
-    */}
-
-    </board>
+  </board>
 )
-
-export default SparkFunMicroModRP2040
