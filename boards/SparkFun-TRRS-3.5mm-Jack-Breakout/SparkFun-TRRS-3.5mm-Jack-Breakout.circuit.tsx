8f1e2e57d9c431785f8fe4b16620b4ee29624495
Rushabh Patil <rushabhpatil.dev@gmail.com>
2026-02-14 02:44:38 +0530

    Add SparkFun TRRS 3.5mm Jack Breakout board files (#255)
    * Add SparkFun TRRS 3.5mm Jack Breakout board files
    
    - Created README.md with product link for SparkFun TRRS 3.5mm Jack Breakout.
    - Added circuit design file SparkFun-TRRS-3.5mm-Jack-Breakout.circuit.tsx.
    - Included 3D snapshot image for the circuit design.
    - Added PCB snapshot SVG for the circuit layout.
    - Created schematic snapshot SVG for the circuit design.
    
    * Fix formatting and spacing in SparkFun TRRS 3.5mm Jack Breakout circuit file
    
    * Update schematic SVG for SparkFun TRRS 3.5mm Jack Breakout: Adjusted transformation matrix and updated connectivity net keys for traces.
    
    * Fix formatting by adding space after self-closing tag for PJ_320D_4A in circuit file

---
+++ boards/SparkFun-TRRS-3.5mm-Jack-Breakout/SparkFun-TRRS-3.5mm-Jack-Breakout.circuit.tsx
@@ -0,0 +1,30 @@
+import { sel } from "tscircuit"
+import { PJ_320D_4A } from "./C95562"
+
+const jumperPinLabels = {
+  pin1: ["Sleeve"],
+  pin2: ["Ring_2"],
+  pin3: ["Ring_1"],
+  pin4: ["Tip"],
+}
+export default () => {
+  return (
+    <board width={"12.70mm"} height={"17.78mm"}>
+      <PJ_320D_4A name="U1" pcbY={4} pcbRotation={90} pcbX={0.8} schX={-2} />
+      <jumper
+        name="J1"
+        schDirection="left"
+        footprint="pinrow4_pinlabeltextalignright_pinlabelorthogonal_doublesidedpinlabel"
+        connections={{
+          pin1: sel.U1.pin1,
+          pin2: sel.U1.pin2,
+          pin3: sel.U1.pin3,
+          pin4: sel.U1.pin4,
+        }}
+        schX={2}
+        pcbY={-7.5}
+        pinLabels={jumperPinLabels}
+      />
+    </board>
+  )
+}
