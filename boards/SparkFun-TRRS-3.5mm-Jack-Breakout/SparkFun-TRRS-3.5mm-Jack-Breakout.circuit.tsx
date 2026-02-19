54f49452cfa2defd8646f873dba5175a8f6ae965
Devesh36 <142524747+Devesh36@users.noreply.github.com>
2026-02-19 16:54:27 +0530

    Refactor PJ_320D_4A symbol structure for improved readability

--- boards/SparkFun-TRRS-3.5mm-Jack-Breakout/SparkFun-TRRS-3.5mm-Jack-Breakout.circuit.tsx
+++ boards/SparkFun-TRRS-3.5mm-Jack-Breakout/SparkFun-TRRS-3.5mm-Jack-Breakout.circuit.tsx
@@ -10,23 +10,60 @@ const jumperPinLabels = {
 export default () => {
   return (
     <board width={"12.70mm"} height={"17.78mm"}>
-      <PJ_320D_4A name="U1" pcbY={4} pcbRotation={90} pcbX={0.8} schX={-2} schRotation={90} 
-      symbol={
-         <symbol>
-        <schematicrect
-          schX={0.1}
-          schY={-0.14}
-          width={0.1}
-          height={0.8}
-          isFilled={true}
-        />
-         <schematicpath strokeWidth={0.02} points={[ { x: 0.5, y: 0.1 },{ x: 0.6, y: 0 }, { x: 0.7, y: 0.1 }, {x: 1, y: 0.1}]} />
-        <schematicpath strokeWidth={0.02} points={[ { x: 0.55, y: -0.1 },{ x: 0.65, y: 0 }, { x: 0.75, y: -0.1 }, {x: 1, y: -0.1}]} />
-        <schematicpath strokeWidth={0.02} points={[{ x: 0.1, y: 0.1 }, { x: 0.1, y: 0.3 }, { x: 1, y: 0.3 } , { x: 1, y: 0.3 }]} />
-        <schematicpath strokeWidth={0.02} points={[{ x: 0.35, y: -0.3 },{ x: 0.45, y: -0.19 }, { x: 0.55, y: -0.3 }, {x: 1, y: -0.3}]}
-        />
-      </symbol>
-      }
+      <PJ_320D_4A
+        name="U1"
+        pcbY={4}
+        pcbRotation={90}
+        pcbX={0.8}
+        schX={-2}
+        schRotation={90}
+        symbol={
+          <symbol>
+            <schematicrect
+              schX={0.1}
+              schY={-0.14}
+              width={0.1}
+              height={0.8}
+              isFilled={true}
+            />
+            <schematicpath
+              strokeWidth={0.02}
+              points={[
+                { x: 0.5, y: 0.1 },
+                { x: 0.6, y: 0 },
+                { x: 0.7, y: 0.1 },
+                { x: 1, y: 0.1 },
+              ]}
+            />
+            <schematicpath
+              strokeWidth={0.02}
+              points={[
+                { x: 0.55, y: -0.1 },
+                { x: 0.65, y: 0 },
+                { x: 0.75, y: -0.1 },
+                { x: 1, y: -0.1 },
+              ]}
+            />
+            <schematicpath
+              strokeWidth={0.02}
+              points={[
+                { x: 0.1, y: 0.1 },
+                { x: 0.1, y: 0.3 },
+                { x: 1, y: 0.3 },
+                { x: 1, y: 0.3 },
+              ]}
+            />
+            <schematicpath
+              strokeWidth={0.02}
+              points={[
+                { x: 0.35, y: -0.3 },
+                { x: 0.45, y: -0.19 },
+                { x: 0.55, y: -0.3 },
+                { x: 1, y: -0.3 },
+              ]}
+            />
+          </symbol>
+        }
       />
       <jumper
         name="J1"
