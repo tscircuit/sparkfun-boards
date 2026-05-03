de138c1a4b2c8a2936bbcf3630d705320af623a8
mohan-bee <mohn08052006@gmail.com>
2026-05-03 23:12:01 +0530

    up

--- boards/SparkFun-RGB-and-Gesture-Sensor/SparkFun-RGB-and-Gesture-Sensor.circuit.tsx
+++ boards/SparkFun-RGB-and-Gesture-Sensor/SparkFun-RGB-and-Gesture-Sensor.circuit.tsx
@@ -1,38 +1,72 @@
-import { Apds9960 } from "./components/Apds9960"
-import { Fiducial } from "./components/Fiducial"
-import { Header1x6 } from "./components/Header1x6"
-import { SolderJumper2, SolderJumper3 } from "./components/SolderJumpers"
-
 const x = (eagleX: number) => `${eagleX - 7.62}mm`
 const y = (eagleY: number) => `${eagleY - 10.16}mm`
 
+const apds9960PinLabels = {
+  pin1: ["pin1", "SDA"],
+  pin2: ["pin2", "INT"],
+  pin3: ["pin3", "LDR"],
+  pin4: ["pin4", "LEDK"],
+  pin5: ["pin5", "LEDA"],
+  pin6: ["pin6", "GND"],
+  pin7: ["pin7", "SCL"],
+  pin8: ["pin8", "VDD"],
+} as const
+
 export default function SparkFunApds9960RgbGestureSensor() {
   return (
-    <board width="15.24mm" height="20.32mm" layers={2} thickness="1.6mm">
-      <Header1x6
+    <board
+      width="15.24mm"
+      height="20.32mm"
+      layers={2}
+      thickness="1.6mm"
+    >
+      <pinheader
         name="JP1"
+        pinCount={6}
+        pinLabels={["VL", "GND", "VCC", "SDA", "SCL", "INT"]}
+        footprint="pinrow6_p2.54_id1.016_od1.8796_nosquareplating_doublesidedpinlabel"
         pcbX={x(7.57)}
         pcbY={y(1.27)}
         schX={-8.5}
         schY={-1.4}
         schWidth="1.1"
         schHeight="1"
+        schPinArrangement={{
+          rightSide: {
+            pins: ["INT", "SCL", "SDA", "VCC", "GND", "VL"],
+            direction: "top-to-bottom",
+          },
+        }}
       />
 
-      <Apds9960
+      <chip
         name="U1"
+        pinLabels={apds9960PinLabels}
+        manufacturerPartNumber="APDS-9960"
+        footprint="kicad:Sensor/Avago_APDS-9960"
+        cadModel={null}
         pcbX={x(7.62)}
         pcbY={y(15.24)}
         schX={6.5}
         schY={0.5}
         schWidth="1.96"
         schHeight="0.8"
+        schPinArrangement={{
+          leftSide: {
+            pins: ["VDD", "GND", "LEDA"],
+            direction: "top-to-bottom",
+          },
+          rightSide: {
+            pins: ["LEDK", "LDR", "INT", "SCL", "SDA"],
+            direction: "top-to-bottom",
+          },
+        }}
       />
       <resistor
         name="R1"
         footprint="0603"
         resistance="10k"
-        pcbX={x(11.43)}
+        pcbX={x(12.7)}
         pcbY={y(13.335)}
         schX={9.7}
         schY={1.45}
@@ -42,7 +76,7 @@ export default function SparkFunApds9960RgbGestureSensor() {
         name="R2"
         footprint="0603"
         resistance="4.7k"
-        pcbX={x(8.89)}
+        pcbX={x(9.3)}
         pcbY={y(10.795)}
         pcbRotation={270}
         schX={-5.2}
@@ -53,7 +87,7 @@ export default function SparkFunApds9960RgbGestureSensor() {
         name="R3"
         footprint="0603"
         resistance="4.7k"
-        pcbX={x(11.43)}
+        pcbX={x(11.3)}
         pcbY={y(10.795)}
         pcbRotation={90}
         schX={-3.6}
@@ -64,7 +98,7 @@ export default function SparkFunApds9960RgbGestureSensor() {
         name="R4"
         footprint="0603"
         resistance="10k"
-        pcbX={x(3.81)}
+        pcbX={x(4)}
         pcbY={y(10.795)}
         pcbRotation={90}
         schX={2.5}
@@ -75,7 +109,7 @@ export default function SparkFunApds9960RgbGestureSensor() {
         name="C2"
         footprint="cap0603"
         capacitance="1uF"
-        pcbX={x(6.52)}
+        pcbX={x(6.2)}
         pcbY={y(10.795)}
         pcbRotation={270}
         schX={4.5}
@@ -104,8 +138,10 @@ export default function SparkFunApds9960RgbGestureSensor() {
         schY={-2.0}
         schOrientation="vertical"
       />
-      <SolderJumper3
+      <solderjumper
         name="SJ1"
+        pinCount={3}
+        footprint="solderjumper3_p0.8128_pw0.635_ph1.27"
         pcbX={x(13.75)}
         pcbY={y(10.795)}
         pcbRotation={270}
@@ -113,8 +149,10 @@ export default function SparkFunApds9960RgbGestureSensor() {
         schY={3.4}
         schHeight="0.4"
       />
-      <SolderJumper2
+      <solderjumper
         name="SJ2"
+        pinCount={2}
+        footprint="solderjumper2_p0.8238_pw0.635_ph1.27"
         pcbX={x(1.27)}
         pcbY={y(10.795)}
         pcbRotation={90}
@@ -122,17 +160,19 @@ export default function SparkFunApds9960RgbGestureSensor() {
         schY={3.0}
       />
 
-      <Fiducial
+      <fiducial
         name="FID1"
+        padDiameter="1mm"
+        soldermaskPullback="1mm"
         pcbX={x(14.224)}
         pcbY={y(4.318)}
-        noSchematicRepresentation
       />
-      <Fiducial
+      <fiducial
         name="FID2"
+        padDiameter="1mm"
+        soldermaskPullback="1mm"
         pcbX={x(5.842)}
         pcbY={y(19.304)}
-        noSchematicRepresentation
       />
       <hole
         name="STANDOFF1"
