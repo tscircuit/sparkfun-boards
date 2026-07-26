be0b4d67e4636d50700b1ad8aa9cce1431086889
anil08607 <anilkumar90875199@gmail.com>
2026-07-26 11:02:10 +0530

    fix: align VCNL4040 and Qwiic connector 3D models with footprints

--- boards/SparkFun-Proximity-Sensor-Breakout-20cm-VCNL4040-(Qwiic)/SM04B_SRSS_TB_LF__SN.tsx
+++ boards/SparkFun-Proximity-Sensor-Breakout-20cm-VCNL4040-(Qwiic)/SM04B_SRSS_TB_LF__SN.tsx
@@ -98,7 +98,7 @@ export const SM04B_SRSS_TB_LF__SN = (props: ChipProps<typeof pinLabels>) => {
         objUrl:
           "https://modelcdn.tscircuit.com/easyeda_models/download?uuid=96ff162c26934a308e7bbf01d083d593&pn=C160404",
         rotationOffset: { x: 0, y: 0, z: 0 },
-        positionOffset: { x: -1.5, y: -0.5, z: 0 },
+        positionOffset: { x: 0, y: 0, z: 0 },
       }}
       {...props}
     />
