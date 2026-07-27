819c7a5fe02be750d5021a7355a107f35fa92e31
Abse2001 <abdaslam13.2.2001@gmail.com>
2026-07-27 13:39:57 +0200

    Add automated JLCPCB footprint conversion tool and migrate boards to footprinter strings

--- boards/SparkFun-RGB-and-Gesture-Sensor/imports/CA45_D_10V_100uF_K.tsx
+++ boards/SparkFun-RGB-and-Gesture-Sensor/imports/CA45_D_10V_100uF_K.tsx
@@ -13,53 +13,7 @@ export const CA45_D_10V_100uF_K = (props: ChipProps<typeof pinLabels>) => {
         jlcpcb: ["C503925"],
       }}
       manufacturerPartNumber="CA45_D_10V_100uF_K"
-      footprint={
-        <footprint>
-          <smtpad
-            portHints={["pin1"]}
-            pcbX="2.9999939999999867mm"
-            pcbY="0mm"
-            width="2.7990799999999996mm"
-            height="3.0099mm"
-            shape="rect"
-          />
-          <smtpad
-            portHints={["pin2"]}
-            pcbX="-2.9999939999999867mm"
-            pcbY="0mm"
-            width="2.7990799999999996mm"
-            height="3.0099mm"
-            shape="rect"
-          />
-          <silkscreenpath
-            route={[
-              { x: 1.099997799999997, y: -2.2999954000000002 },
-              { x: 1.101267799999988, y: 2.2999954000000002 },
-            ]}
-          />
-          <silkscreenpath
-            route={[
-              { x: -3.6999926000000016, y: 2.2999954000000002 },
-              { x: 3.5999928000000097, y: 2.2999954000000002 },
-            ]}
-          />
-          <silkscreenpath
-            route={[
-              { x: 3.6999925999999874, y: -2.2999954000000002 },
-              { x: -3.5999927999999954, y: -2.2999954000000002 },
-            ]}
-          />
-          <courtyardoutline
-            outline={[
-              { x: -4.644199999999998, y: 2.561400000000006 },
-              { x: 4.644200000000012, y: 2.561400000000006 },
-              { x: 4.644200000000012, y: -2.5360000000000014 },
-              { x: -4.644199999999998, y: -2.5360000000000014 },
-              { x: -4.644199999999998, y: 2.561400000000006 },
-            ]}
-          />
-        </footprint>
-      }
+      footprint="res_p6mm_pw2.7991mm_ph3.0099mm_pin1location(rightside,top)"
       cadModel={{
         objUrl:
           "https://modelcdn.tscircuit.com/easyeda_models/assets/C503925.obj?uuid=54b4a7b9b595470ba3bec66fde072b82",
