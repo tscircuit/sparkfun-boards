f274f3403a02a87edd939f0879c43457aa19463f
hrithik18k <hrithikvasanthram@gmail.com>
2026-08-18 12:24:50 +0530

    refine AP3429A schematic layout

--- boards/SparkFun-Buck-Regulator-AP3429A/SparkFun-Buck-Regulator-AP3429A.circuit.tsx
+++ boards/SparkFun-Buck-Regulator-AP3429A/SparkFun-Buck-Regulator-AP3429A.circuit.tsx
@@ -2,14 +2,10 @@ import React from "react"
 
 const X = (x: number) => x - 12.7
 const Y = (y: number) => y - 12.7
-
 const ThroughHoleRow = ({
   count,
   pitch = 2.54,
-}: {
-  count: number
-  pitch?: number
-}) => (
+}: { count: number; pitch?: number }) => (
   <footprint>
     {Array.from({ length: count }, (_, i) => (
       <React.Fragment key={i}>
@@ -25,7 +21,6 @@ const ThroughHoleRow = ({
     ))}
   </footprint>
 )
-
 const BarrelJackFootprint = () => (
   <footprint>
     <platedhole
@@ -75,7 +70,12 @@ const BarrelJackFootprint = () => (
 )
 
 export default () => (
-  <board width="25.4mm" height="25.4mm" layers={2}>
+  <board
+    width="25.4mm"
+    height="25.4mm"
+    layers={2}
+    schTraceAutoLabelEnabled={false}
+  >
     <schematicsection
       name="Power Conversion"
       displayName="Adjustable DC-DC Buck Regulator - AP3429A"
@@ -103,6 +103,20 @@ export default () => (
       fontSize={0.18}
       color="gray"
     />
+    <schematictext
+      text="Cut trace to disconnect LED."
+      schX={26.5}
+      schY={3}
+      fontSize={0.18}
+      color="gray"
+    />
+    <schematictext
+      text="WARNING: Only connect one Vin input at a time"
+      schX={0}
+      schY={-14}
+      fontSize={0.18}
+      color="gray"
+    />
     <chip
       name="U1"
       manufacturerPartNumber="AP3429A"
@@ -114,62 +128,15 @@ export default () => (
         pin4: "VIN",
         pin5: "FB",
       }}
-      symbol={
-        <symbol width={1.75} height={2.4}>
-          <schematicrect
-            schX={0}
-            schY={0}
-            width={1.75}
-            height={2.4}
-            strokeWidth={0.015}
-            color="red"
-            isFilled={false}
-          />
-          <schematictext text="VIN" schX={-0.55} schY={0.62} fontSize={0.18} />
-          <schematictext text="EN" schX={-0.55} schY={0} fontSize={0.18} />
-          <schematictext text="GND" schX={-0.38} schY={-0.68} fontSize={0.18} />
-          <schematictext text="LX" schX={0.55} schY={0.62} fontSize={0.18} />
-          <schematictext text="FB" schX={0.55} schY={-0.25} fontSize={0.18} />
-          <port
-            name="VIN"
-            pinNumber={4}
-            direction="left"
-            schX={-0.875}
-            schY={0.62}
-          />
-          <port
-            name="EN"
-            pinNumber={1}
-            direction="left"
-            schX={-0.875}
-            schY={0}
-          />
-          <port
-            name="GND"
-            pinNumber={2}
-            direction="down"
-            schX={-0.4}
-            schY={-1.2}
-          />
-          <port
-            name="SW"
-            pinNumber={3}
-            direction="right"
-            schX={0.875}
-            schY={0.62}
-          />
-          <port
-            name="FB"
-            pinNumber={5}
-            direction="right"
-            schX={0.875}
-            schY={-0.25}
-          />
-        </symbol>
-      }
+      schPinArrangement={{
+        leftSide: { pins: ["VIN", "EN", "GND"], direction: "top-to-bottom" },
+        rightSide: { pins: ["SW", "FB"], direction: "top-to-bottom" },
+      }}
+      schWidth={1.675}
+      schHeight={0.6}
       pcbX={X(15.24)}
       pcbY={Y(13.335)}
-      schX={-0.06}
+      schX={0}
       schY={1}
       schSectionName="Power Conversion"
     />
@@ -179,7 +146,7 @@ export default () => (
       footprint="1206"
       pcbX={X(16.256)}
       pcbY={Y(8.509)}
-      schX={3.06}
+      schX={3}
       schY={1}
       schSectionName="Power Conversion"
     />
@@ -188,11 +155,11 @@ export default () => (
       capacitance="22uF"
       footprint="0805"
       pcbX={X(19.2)}
-      pcbY={Y(13.489)}
+      pcbY={Y(13.589)}
       pcbRotation={270}
       schOrientation="vertical"
       schX={-5}
-      schY={0}
+      schY={0.7}
       schSectionName="Power Conversion"
     />
     <capacitor
@@ -203,8 +170,8 @@ export default () => (
       pcbY={Y(7.5)}
       pcbRotation={90}
       schOrientation="vertical"
-      schX={5}
-      schY={0}
+      schX={4.5}
+      schY={0.7}
       schSectionName="Power Conversion"
     />
     <capacitor
@@ -216,7 +183,7 @@ export default () => (
       pcbRotation={90}
       schOrientation="vertical"
       schX={9}
-      schY={0}
+      schY={0.7}
       schSectionName="Power Conversion"
     />
     <capacitor
@@ -228,7 +195,7 @@ export default () => (
       pcbRotation={270}
       schOrientation="vertical"
       schX={11}
-      schY={0}
+      schY={0.7}
       schSectionName="Power Conversion"
     />
     <capacitor
@@ -240,19 +207,19 @@ export default () => (
       pcbRotation={270}
       schOrientation="vertical"
       schX={13}
-      schY={0}
+      schY={0.7}
       schSectionName="Power Conversion"
     />
     <capacitor
       name="C6"
       capacitance="1uF"
       footprint="0603"
-      pcbX={X(14)}
+      pcbX={X(14.0)}
       pcbY={Y(5.842)}
       pcbRotation={270}
       schOrientation="vertical"
       schX={15}
-      schY={0}
+      schY={0.7}
       schSectionName="Power Conversion"
     />
     <resistor
@@ -263,8 +230,8 @@ export default () => (
       pcbY={Y(11.5)}
       pcbRotation={270}
       schOrientation="vertical"
-      schX={7}
-      schY={0}
+      schX={6}
+      schY={0.7}
       schSectionName="Power Conversion"
     />
     <resistor
@@ -274,8 +241,8 @@ export default () => (
       pcbX={X(9.2)}
       pcbY={Y(13.5)}
       schOrientation="vertical"
-      schX={7}
-      schY={-3}
+      schX={6}
+      schY={-1.4}
       schSectionName="Power Conversion"
     />
     <resistor
@@ -286,7 +253,6 @@ export default () => (
       pcbY={Y(2.667)}
       pcbRotation={180}
       schOrientation="vertical"
-      schRotation={180}
       schX={24}
       schY={1}
       schSectionName="Indicator"
@@ -295,12 +261,12 @@ export default () => (
       name="R4"
       resistance="100k"
       footprint="0603"
-      pcbX={X(20.458)}
+      pcbX={X(19.558)}
       pcbY={Y(16.891)}
       pcbRotation={270}
       schOrientation="vertical"
       schX={-3}
-      schY={0}
+      schY={0.7}
       schSectionName="Power Conversion"
     />
     <led
@@ -310,7 +276,7 @@ export default () => (
       pcbX={X(18.034)}
       pcbY={Y(4.318)}
       pcbRotation={180}
-      schRotation={90}
+      schRotation={270}
       schX={24}
       schY={-1}
       schSectionName="Indicator"
@@ -320,16 +286,23 @@ export default () => (
       manufacturerPartNumber="Screw Terminal 4-way"
       footprint={<ThroughHoleRow count={4} pitch={3.5} />}
       pinLabels={{ pin1: "VIN", pin2: "GND1", pin3: "GND2", pin4: "VOUT" }}
+      schPinArrangement={{
+        rightSide: {
+          pins: ["VOUT", "GND2", "GND1", "VIN"],
+          direction: "top-to-bottom",
+        },
+      }}
+      schWidth={1.48}
       pcbX={X(3.5)}
       pcbY={Y(14.2)}
       pcbRotation={270}
-      schX={-5}
+      schX={-6}
       schY={-10}
       schSectionName="Connectors"
     />
     <chip
       name="J2"
-      manufacturerPartNumber="DC Barrel Jack"
+      manufacturerPartNumber="POWER_JACK"
       footprint={<BarrelJackFootprint />}
       pinLabels={{ pin1: "PWR", pin2: "GND", pin3: "GNDBREAK" }}
       schPinArrangement={{
@@ -338,8 +311,9 @@ export default () => (
           direction: "top-to-bottom",
         },
       }}
+      schWidth={1.8}
       pcbX={X(13.335)}
-      pcbY={Y(23.4)}
+      pcbY={Y(24.003)}
       pcbRotation={90}
       schX={0}
       schY={-10}
@@ -357,8 +331,8 @@ export default () => (
         pin5: "VOUT",
       }}
       schPinArrangement={{
-        leftSide: {
-          pins: ["EN", "VIN", "GND1", "GND2", "VOUT"],
+        rightSide: {
+          pins: ["VOUT", "GND2", "GND1", "VIN", "EN"],
           direction: "top-to-bottom",
         },
       }}
@@ -366,7 +340,7 @@ export default () => (
       pcbX={X(23.5)}
       pcbY={Y(14.5)}
       pcbRotation={270}
-      schX={5}
+      schX={6}
       schY={-10}
       schSectionName="Connectors"
     />
@@ -399,6 +373,90 @@ export default () => (
       soldermaskPullback="0.4mm"
       pcbX={X(0.762)}
       pcbY={Y(24.638)}
+    />
+    <netlabel
+      net="VIN"
+      connection="C1.pin1"
+      schX={-6}
+      schY={3}
+      anchorSide="bottom"
+    />
+    <netlabel
+      net="VOUT"
+      connection="C6.pin1"
+      schX={16}
+      schY={3}
+      anchorSide="bottom"
+    />
+    <netlabel
+      net="EN"
+      connection="R4.pin1"
+      schX={-3.8}
+      schY={-1}
+      anchorSide="right"
+    />
+    <netlabel
+      net="GND"
+      connection="C1.pin2"
+      schX={-5}
+      schY={-2.5}
+      anchorSide="top"
+    />
+    <netlabel
+      net="GND"
+      connection="U1.GND"
+      schX={-0.5}
+      schY={-2.5}
+      anchorSide="top"
+    />
+    <netlabel
+      net="GND"
+      connection="R2.pin2"
+      schX={6}
+      schY={-2.5}
+      anchorSide="top"
+    />
+    <netlabel
+      net="GND"
+      connection="C3.pin2"
+      schX={9}
+      schY={-2.5}
+      anchorSide="top"
+    />
+    <netlabel
+      net="GND"
+      connection="C4.pin2"
+      schX={11}
+      schY={-2.5}
+      anchorSide="top"
+    />
+    <netlabel
+      net="GND"
+      connection="C5.pin2"
+      schX={13}
+      schY={-2.5}
+      anchorSide="top"
+    />
+    <netlabel
+      net="GND"
+      connection="C6.pin2"
+      schX={15}
+      schY={-2.5}
+      anchorSide="top"
+    />
+    <netlabel
+      net="VOUT"
+      connection="PWR.pin2"
+      schX={24}
+      schY={4.5}
+      anchorSide="bottom"
+    />
+    <netlabel
+      net="GND"
+      connection="D1.cathode"
+      schX={24}
+      schY={-2.8}
+      anchorSide="top"
     />
     <silkscreenrect
       pcbX={X(6.35)}
@@ -434,43 +492,31 @@ export default () => (
       height="0.2mm"
       layer="top"
     />
-    <trace from="U1.VIN" to="net.VIN" />
-    <trace from="U1.GND" to="net.GND" />
-    <trace from="U1.EN" to="net.EN" />
+    <trace from="C1.pin1" to="R4.pin2" />
+    <trace from="R4.pin2" to="U1.VIN" />
     <trace from="U1.SW" to="L1.pin1" />
     <trace from="L1.pin2" to="net.VOUT" />
-    <trace from="U1.FB" to="C2.pin2" />
-    <trace from="U1.FB" to="R1.pin1" />
-    <trace from="U1.FB" to="R2.pin2" />
-    <trace from="R1.pin2" to="net.VOUT" />
     <trace from="C2.pin1" to="net.VOUT" />
-    <trace from="R2.pin1" to="net.GND" />
-    <trace from="C1.pin1" to="net.VIN" />
-    <trace from="C1.pin2" to="net.GND" />
+    <trace from="R1.pin1" to="net.VOUT" />
     <trace from="C3.pin1" to="net.VOUT" />
-    <trace from="C3.pin2" to="net.GND" />
     <trace from="C4.pin1" to="net.VOUT" />
-    <trace from="C4.pin2" to="net.GND" />
     <trace from="C5.pin1" to="net.VOUT" />
-    <trace from="C5.pin2" to="net.GND" />
-    <trace from="C6.pin1" to="net.VOUT" />
-    <trace from="C6.pin2" to="net.GND" />
-    <trace from="R4.pin1" to="net.EN" />
-    <trace from="R4.pin2" to="net.VIN" />
-    <trace from="D1.anode" to="R3.pin1" />
-    <trace from="R3.pin2" to="PWR.pin1" />
-    <trace from="PWR.pin2" to="net.VOUT" />
-    <trace from="D1.cathode" to="net.GND" />
+    <trace from="U1.FB" to="C2.pin2" schDisplayLabel="FB" />
+    <trace from="C2.pin2" to="R1.pin2" schDisplayLabel="FB" />
+    <trace from="R1.pin2" to="R2.pin1" schDisplayLabel="FB" />
+    <trace from="R4.pin1" to="U1.EN" />
+    <trace from="PWR.pin1" to="R3.pin1" />
+    <trace from="R3.pin2" to="D1.anode" />
+    <trace from="J1.VOUT" to="net.VOUT" />
     <trace from="J1.VIN" to="net.VIN" />
     <trace from="J1.GND1" to="net.GND" />
-    <trace from="J1.GND2" to="net.GND" />
-    <trace from="J1.VOUT" to="net.VOUT" />
+    <trace from="J1.GND2" to="J1.GND1" />
     <trace from="J2.PWR" to="net.VIN" />
     <trace from="J2.GND" to="net.GND" />
-    <trace from="J3.EN" to="net.EN" />
+    <trace from="J3.VOUT" to="net.VOUT" />
     <trace from="J3.VIN" to="net.VIN" />
     <trace from="J3.GND1" to="net.GND" />
-    <trace from="J3.GND2" to="net.GND" />
-    <trace from="J3.VOUT" to="net.VOUT" />
+    <trace from="J3.GND2" to="J3.GND1" />
+    <trace from="J3.EN" to="net.EN" />
   </board>
 )
